281f1f7261a85bbf3ec62c8c71e9dc4b7544d845
Tobias Käs <tobias.kaes@gmail.com>
2020-08-31 00:32:29 +0200

    remove things I'm not interested in

--- azure-pipelines/variables/ProfilingInputsDropName.ps1
+++
@@ -1,5 +0,0 @@
-if ($env:System_TeamProject) {
-    "ProfilingInputs/$env:System_TeamProject/$env:Build_Repository_Name/$env:Build_SourceBranchName/$env:Build_BuildId"
-} else {
-    Write-Warning "No Azure Pipelines build detected. No VSTS drop name will be computed."
-}
--- src/SosThreadingTools/ICommandHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-
-namespace CpsDbg
-{
-    internal interface ICommandHandler
-    {
-        void Execute(DebuggerContext context, string args);
-    }
-}

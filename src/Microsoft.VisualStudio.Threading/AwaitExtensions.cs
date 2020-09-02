de78f1e6fc2a83185f922173d7aba93ef9291065
Tobias Käs <tobias.kaes@gmail.com>
2020-09-02 15:07:24 +0200

    remove WaitForExitAsync

--- src/Microsoft.VisualStudio.Threading/AwaitExtensions.cs
+++ src/Microsoft.VisualStudio.Threading/AwaitExtensions.cs
@@ -60,46 +60,6 @@ namespace Microsoft.VisualStudio.Threading
         }
 
         /// <summary>
-        /// Returns a task that completes when the process exits and provides the exit code of that process.
-        /// </summary>
-        /// <param name="process">The process to wait for exit.</param>
-        /// <param name="cancellationToken">
-        /// A token whose cancellation will cause the returned Task to complete
-        /// before the process exits in a faulted state with an <see cref="OperationCanceledException"/>.
-        /// This token has no effect on the <paramref name="process"/> itself.
-        /// </param>
-        /// <returns>A task whose result is the <see cref="Process.ExitCode"/> of the <paramref name="process"/>.</returns>
-        public static async Task<int> WaitForExitAsync(this Process process, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            Requires.NotNull(process, nameof(process));
-
-            var tcs = new TaskCompletionSource<int>();
-            EventHandler exitHandler = (s, e) =>
-            {
-                tcs.TrySetResult(process.ExitCode);
-            };
-            try
-            {
-                process.EnableRaisingEvents = true;
-                process.Exited += exitHandler;
-                if (process.HasExited)
-                {
-                    // Allow for the race condition that the process has already exited.
-                    tcs.TrySetResult(process.ExitCode);
-                }
-
-                using (cancellationToken.Register(() => tcs.TrySetCanceled(cancellationToken)))
-                {
-                    return await tcs.Task.ConfigureAwait(false);
-                }
-            }
-            finally
-            {
-                process.Exited -= exitHandler;
-            }
-        }
-
-        /// <summary>
         /// Converts a <see cref="YieldAwaitable"/> to a <see cref="ConfiguredTaskYieldAwaitable"/>.
         /// </summary>
         /// <param name="yieldAwaitable">The result of <see cref="Task.Yield()"/>.</param>
